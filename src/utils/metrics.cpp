6bc765349a04df46e85258fb817fc8c16f7e3896
Édipo da Silva Souza <edipo2s@gmail.com>
2019-07-18 13:51:33 -0300

    Do not emit influx metrics in debug mode

--- src/utils/metrics.cpp
+++ src/utils/metrics.cpp
@@ -3,8 +3,12 @@
 #include "metrics.h"
 
 void influx_metric(influxdb_cpp::detail::tag_or_field_caller &builder) {
+#ifdef QT_DEBUG
+    return;
+#else
     builder
         .tag("version", LOTUS_TRACKER->applicationVersion().toStdString())
         .field("user", LOTUS_TRACKER->appSettings->getInstallationUuid().toStdString())
         .send_udp("metrics.hearthsim.net", 8094);
+#endif
 }
